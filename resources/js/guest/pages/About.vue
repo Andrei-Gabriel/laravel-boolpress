87b0bad6309700b0380c4bc5d6b536a7c486fb7c
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-25 16:45:16 +0100

    Aggiunto about

--- resources/js/guest/pages/About.vue
+++ resources/js/guest/pages/About.vue
@@ -1,10 +1,10 @@
 <template>
-    <h2>Home Page</h2>
+    <h2>About</h2>
 </template>
 
 <script>
     export default {
-        name: "Home",
+        name: "About",
     }  
 </script>
 
--- resources/js/guest/router.js
+++ resources/js/guest/router.js
@@ -5,6 +5,8 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 import Home from "./pages/Home";
+import About from "./pages/About";
+
 const router = new VueRouter({
     mode: "history",
     routes: [
@@ -12,6 +14,11 @@ const router = new VueRouter({
             path: "/",
             name: "home",
             component: Home
+        },
+        {
+            path: "/chi-siamo",
+            name: "about",
+            component: About
         },
     ]
 });
